3dbb01d9791739cf6a5b0edad8f3437700f17507
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-16 15:57:19 -0500

    added testing for display file

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,6 +3,6 @@ import React from 'react';
 import Dashboard from './Dashboard';
 import { render } from '@testing-library/react';
 
-test('Dashboard renders properly', () => {
+test('Dashboard renders correctly', () => {
     render(<Dashboard />);
   });
