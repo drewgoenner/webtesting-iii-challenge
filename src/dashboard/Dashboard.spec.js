a7af3f2afe3dee0748dabbe431a72da871c7cb7e
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-16 14:27:47 -0500

    passes test to verify that the dashboard is rendering properly

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
 // Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import { render } from '@testing-library/react';
+
+test('Dashboard renders properly', () => {
+    render(<Dashboard />);
+  });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+import { render } from '@testing-library/react';
+
+test('Display renders properly', () => {
+    render(<Dashboard />);
+  });
